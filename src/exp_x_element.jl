e0af77a78944476d3035b9d51ec5653453b41a27
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-12-31 18:16:59 -0500

    Added a function for creating the initial EXP_X tuples and tested it.

--- src/exp_x_element.jl
+++ src/exp_x_element.jl
@@ -2,7 +2,10 @@
 #Description:
 #   
 
-const EXP_X_Element = NamedTuple{(:v,:q,:c)}
+const EXP_X_Element = NamedTuple{
+    (:v,:q,:c),
+    Tuple{Vector{String},Vector{String},Vector{String}}
+    }
 
 # Functions
 
@@ -11,7 +14,7 @@ GetInitialEXP_X
 Description:
     Creates the initial EXP_X set which has a simple external behavior sequence.
 """
-function GetInitialEXP_X(system_in::System,cover_in::Vector{Vector{String}})
+function GetInitialEXP_X( system_in::System , cover_in::Vector{Vector{String}} )
     # Constants
 
     # Algorithm
@@ -25,10 +28,27 @@ function GetInitialEXP_X(system_in::System,cover_in::Vector{Vector{String}})
 
         # Push into Array
         if length(temp_c) > 0
-            push!(tempEXP_X,EXP_X_Element( ( Vector{String}([temp_y]),temp_q,convert(Vector{String},temp_c) ) ) )
+            push!(tempEXP_X,EXP_X_Element( ( Vector{String}([temp_y]),temp_q,temp_c ) ) )
         end
     end
 
     return tempEXP_X
+end
 
+"""
+ProjectToGammaSet
+Description:
+    Projects all of the elements in a set of EXP_X_Element elements to a set of 
+"""
+function ProjectToGammaSet( X_set::Vector{EXP_X_Element} )
+    # Constants
+
+    # Algorithm
+    gamma_set_out = Vector{EXP_Gamma_Element}([])
+    for x_elt in X_set
+        # Append to the new Gamma Set each Projected element
+        push!(gamma_set_out,EXP_Gamma_Element(x_elt))
+    end
+
+    return gamma_set_out
 end
--- test/exp_x_element_test.jl
+++ test/exp_x_element_test.jl
@@ -4,10 +4,7 @@
 
 using Test
 
-include("system.jl")
-include("cover.jl")
-include("exp_x_element.jl")
-
+include("../src/KAM.jl")
 
 """
 Test 1: v,q, and c
@@ -45,13 +42,65 @@ sys1a = System(
 )
 
 cover1 = CreateCover(sys1a)
-println( Set(["q0","q2","q3","q2"]) )
-for x in Set(["q0","q2","q3","q4"])
-    println(x)
-end
-println( convert(Vector{String},Set(["q0","q2","q3","q4"])) )
-
 EXP_X1 = GetInitialEXP_X(sys1a,cover1)
 
 @test length(EXP_X1) == 1
 @test EXP_X1[1] == EXP_X_Element( ( Vector{String}(["y2"]) , Vector{String}(["q0"]) , Vector{String}(["q0"]) ) )
+
+# Test 2: Multiple initial states, with same output
+
+sys2 = System(
+    ["q0","q1","q2","q3"], 
+    ["q0","q2"], 
+    ["o1","o2"], 
+    [
+        sparse([2,2],[2,3],[1,1]),
+        sparse([1,2,2],[2,2,3],[1,1,1]),
+        sparse([1,1,2],[2,4,3],[1,1,1]),
+        sparse([1,2],[1,2],[1,1])
+    ],
+    ["y1","y2","y3"],
+    sparse([1,2,3,4],[2,3,2,1],[1,1,1,1])
+)
+
+cover2 = CreateCover(sys2)
+EXP_X2= GetInitialEXP_X(sys2,cover2)
+
+@test length(EXP_X2) == 1
+@test EXP_X2[1] == EXP_X_Element( ( Vector{String}(["y2"]) , Vector{String}(["q0","q2"]) , Vector{String}(["q0","q2"]) ) )
+
+# Test 3: Multiple initial states, with different outputs
+
+sys3 = System(
+    ["q0","q1","q2","q3","q4"], 
+    ["q0","q1","q2"], 
+    ["o1","o2"], 
+    [
+        sparse([2,2],[2,3],[1,1]),
+        sparse([1,2,2],[2,2,3],[1,1,1]),
+        sparse([1,1,2],[2,4,3],[1,1,1]),
+        sparse([1,2],[1,2],[1,1])
+    ],
+    ["y1","y2","y3"],
+    sparse([1,2,3,4,5],[2,3,2,1,2],[1,1,1,1,1])
+)
+
+cover3 = CreateCover(sys3)
+EXP_X3= GetInitialEXP_X(sys3,cover3)
+
+@test length(EXP_X3) == 2
+@test EXP_X3[1] == EXP_X_Element( ( Vector{String}(["y2"]) , Vector{String}(["q0","q2","q4"]) , Vector{String}(["q0","q2"]) ) )
+@test EXP_X3[2] == EXP_X_Element( ( Vector{String}(["y3"]) , Vector{String}(["q1"]) , Vector{String}(["q1"]) ) )
+
+"""
+Test 3: ProjectToGammaSet
+"""
+
+exp_x1 = EXP_X_Element( (Vector{String}(["woah"]),["typo"],["typo"]) )
+exp_x2 = EXP_X_Element( (["tvShow"],["walkingDead"],["walkingDead"]) )
+
+EXP_X1 = Vector{EXP_X_Element}([exp_x1,exp_x2])
+project1 = ProjectToGammaSet(EXP_X1)
+
+@test project1[1] == EXP_Gamma_Element( (["typo"],["typo"]) )
+@test project1[2] == EXP_Gamma_Element( (["walkingDead"],["walkingDead"]) )
